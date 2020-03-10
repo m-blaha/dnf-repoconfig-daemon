3f9ec76da94f97f574a8ff418158b772b35b2b8e
Marek Blaha <mblaha@redhat.com>
2020-03-10 09:29:25 +0100

    Initial test

---
+++ test/Context.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "Context.hpp"
+
+#include "libdnf/conf/ConfigMain.hpp"
+#include "libdnf/conf/ConfigParser.hpp"
+#include "libdnf/conf/ConfigRepo.hpp"
+#include "libdnf/dnf-context.h" // for find_base_arch
+#include "libdnf/repo/Repo.hpp"
+
+#include <glob.h>
+#include <iostream>
+#include <rpm/rpmlib.h>
+
+bool Context::libRpmInitiated = false;
+
+void Context::configure()
+{
+    setSubstitutions();
+    readMainConfig();
+    readRepoConfigs();
+}
+
+void Context::setSubstitutions()
+{
+    if (!libRpmInitiated) {
+        if (rpmReadConfigFiles(NULL, NULL) != 0) {
+            throw std::runtime_error("Failed to read rpm config files\n");
+        }
+        libRpmInitiated = true;
+    }
+    const char *arch;
+    rpmGetArchInfo(&arch, NULL);
+    substitutions["arch"] = std::string(arch);
+    const auto basearch = find_base_arch(arch);
+    substitutions["basearch"] = std::string(basearch);
+    //TODO code for detecting releasever
+    substitutions["releasever"] = "31";
+}
+
+void Context::readMainConfig()
+{
+    cfgMainParser.setSubstitutions(substitutions);
+    cfgMainParser.read(cfgMain.config_file_path().getValue());
+    const auto & cfgParserData = cfgMainParser.getData();
+    const auto cfgParserDataIter = cfgParserData.find("main");
+    if (cfgParserDataIter != cfgParserData.end()) {
+        auto optBinds = cfgMain.optBinds();
+        for (const auto &opt : cfgParserDataIter->second) {
+            const auto optBindsIter = optBinds.find(opt.first);
+            if (optBindsIter != optBinds.end()) {
+                optBindsIter->second.newString(
+                    libdnf::Option::Priority::MAINCONFIG,
+                    cfgMainParser.getSubstitutedValue("main", opt.first));
+            }
+        }
+    }
+}
+
+void Context::readRepoConfigs()
+{
+    for (auto reposDir : cfgMain.reposdir().getValue()) {
+        const std::string pattern = reposDir + "/*.repo";
+        std::cout << "XXX reposdir: " << reposDir << std::endl;
+        std::cout << "XXX pattern: " << pattern << std::endl;
+
+        glob_t globResult;
+        glob(pattern.c_str(), GLOB_MARK, NULL, &globResult);
+        for (size_t i = 0; i < globResult.gl_pathc; ++i) {
+            std::cout << "XXX file: " << globResult.gl_pathv[i] << std::endl;
+            std::unique_ptr<libdnf::ConfigParser> repo_parser(new libdnf::ConfigParser);
+            repo_parser->setSubstitutions(substitutions);
+            repo_parser->read(globResult.gl_pathv[i]);
+            const auto & cfgParserData = repo_parser->getData();
+            for (const auto & cfgParserDataIter : cfgParserData) {
+                if (cfgParserDataIter.first != "main") {
+                    auto section = cfgParserDataIter.first;
+                    std::cout << "XXX section: " << section << std::endl;
+                    std::unique_ptr<libdnf::ConfigRepo> cfgRepo(new libdnf::ConfigRepo(cfgMain));
+                    auto optBinds = cfgRepo->optBinds();
+                    for (const auto &opt : cfgParserDataIter.second) {
+                        if (opt.first[0] == '#') {
+                            continue;
+                        }
+                        const auto optBindsIter = optBinds.find(opt.first);
+                        if (optBindsIter != optBinds.end()) {
+                            optBindsIter->second.newString(
+                                libdnf::Option::Priority::REPOCONFIG,
+                                repo_parser->getSubstitutedValue(section, opt.first));
+                        }
+                    }
+                    std::cout << "XXX name: " << cfgRepo->name().getValue() << std::endl;
+                    for (auto & bu : cfgRepo->baseurl().getValue()) {
+                        std::cout << "XXX baseurl: " << bu << std::endl;
+                    }
+                    std::unique_ptr<libdnf::Repo> repo(new libdnf::Repo(section, std::move(cfgRepo)));
+                    repos.push_back(std::move(repo));
+                }
+            }
+            cfgRepoParsers.push_back(std::move(repo_parser));
+        }
+        globfree(&globResult);
+        std::cout << "XXX ----" << std::endl;
+    }
+}
---
+++ test/Context.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef DNFDAEMON_CONTEXT_HPP
+#define DNFDAEMON_CONTEXT_HPP
+
+#include "libdnf/conf/ConfigMain.hpp"
+#include "libdnf/conf/ConfigParser.hpp"
+#include "libdnf/conf/ConfigRepo.hpp"
+#include "libdnf/repo/Repo.hpp"
+
+#include <map>
+#include <memory>
+#include <vector>
+
+class Context {
+public:
+    libdnf::ConfigMain cfgMain;
+    std::vector<std::unique_ptr<libdnf::Repo>> repos;
+
+    void configure();
+
+private:
+    libdnf::ConfigParser cfgMainParser;
+    std::vector<std::unique_ptr<libdnf::ConfigParser>> cfgRepoParsers;
+    std::map<std::string, std::string> substitutions;
+    static bool libRpmInitiated;
+
+    void setSubstitutions();
+    void readMainConfig();
+    void readRepoConfigs();
+};
+
+
+#endif
