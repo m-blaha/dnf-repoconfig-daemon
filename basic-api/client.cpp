3e7e0926aa3f7eaf566053b72e1f5347d2dc3442
Marek Blaha <mblaha@redhat.com>
2020-03-26 14:23:39 +0100

    Fix example client

--- basic-api/client.cpp
+++ basic-api/client.cpp
@@ -16,11 +16,15 @@ int main(int argc, char *argv[])
 
     // Invoke test on given interface of the object
     {
-        auto method = repoconf_proxy->createMethodCall(interfaceName, "test");
-        auto reply = repoconf_proxy->callMethod(method);
-        std::string result;
-        reply >> result;
-        std::cout << "Result: " << result << std::endl;
+        std::vector<std::string> ids = {"fedora", "updates"};
+        std::vector<std::map<std::string, sdbus::Variant>> repolist;
+        repoconf_proxy->callMethod("list").onInterface(interfaceName).withArguments(ids).storeResultsTo(repolist);
+        for (auto &repo: repolist) {
+            for (auto &item: repo) {
+                std::cout << item.first << ": " << item.second.get<std::string>() << std::endl;
+            }
+            std::cout << "-----------" << std::endl;
+        }
     }
 
     return 0;
